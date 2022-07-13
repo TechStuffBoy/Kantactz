1897b750f412896a16ae7a7a6de39f7d28c9833b
TechStuffBoy <nandhu03.m@gmail.com>
2022-07-13 21:23:50 +0530

    Add all the basic CRUD views and templates.

--- addressbook/urls.py
+++ addressbook/urls.py
@@ -3,5 +3,9 @@ from . import views
 
 urlpatterns = [
     path('home/', view=views.HomeView.as_view(), name='home'),
-    path('', view=views.ContactList.as_view(), name='contact-list')
+    path('', view=views.ContactList.as_view(), name='contact-list'),
+    path('<int:pk>/', view=views.ContactDetail.as_view(), name='contact-detail'),
+    path('create/', view=views.ContactCreate.as_view(), name='contact-add'),
+    path('<int:pk>/edit/', view=views.ContactUpdate.as_view(), name='contact-edit'),
+    path('<int:pk>/delete/', view=views.ContactDelete.as_view(), name='contact-delete'),
 ]
--- addressbook/views.py
+++ addressbook/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
-from django.views.generic import TemplateView, ListView
+from django.views.generic import TemplateView, ListView, DetailView, CreateView, UpdateView, DeleteView
+from django.db.models import Q
+from django.urls import reverse
+from django.contrib import messages
+
+
 from .models import Contact
 
 # Create your views here.
@@ -12,4 +17,53 @@ class ContactList(ListView):
     paginate_by = 2
 
     def get_queryset(self):
-        return super().get_queryset().filter(creator=self.request.user)
+        query = self.request.GET.get('q')
+        if query:
+            return Contact.objects.filter(
+                Q(name__icontains=query) | 
+                Q(number__icontains=query) |
+                Q(email__icontains=query) |
+                Q(email2__icontains=query)
+            ).filter(creator=self.request.user)
+        else:
+            return super().get_queryset().filter(creator=self.request.user)
+
+class ContactDetail(DetailView):
+    model = Contact
+    context_object_name = 'contact'
+
+
+class ContactCreate(CreateView):
+    model = Contact
+    fields = ['name' , 'number', 'country_code', 'email', 'email2']
+    action = 'Add'
+
+    def get_success_url(self):
+        messages.success(self.request,'Contact Created!')
+        return reverse('contact-list')
+    
+    def form_valid(self, form):
+        form = form.save(commit=False)
+        form.creator = self.request.user # Update the user here
+        return super().form_valid(form)
+
+class ContactUpdate(UpdateView):
+    model = Contact
+    fields = ['name' , 'number', 'country_code', 'email', 'email2']
+    action = 'Edit'
+
+    def get_success_url(self):
+        messages.success(self.request,'Details Updated!')
+        return reverse(
+            "contact-detail",
+            kwargs={'pk':self.kwargs['pk']}
+        )
+
+
+class ContactDelete(DeleteView):
+    model = Contact
+
+    def get_success_url(self):
+        messages.success(self.request,'Contact Deleted Successfully!')
+        return reverse('contact-list')
+
