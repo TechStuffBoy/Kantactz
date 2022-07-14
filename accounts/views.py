f74eab2c76432967262349e1472975586d16d945
TechStuffBoy <nandhu03.m@gmail.com>
2022-07-14 19:34:47 +0530

    Add email field to UserCreationForm.

--- accounts/views.py
+++ accounts/views.py
@@ -3,7 +3,17 @@ from django.contrib.auth.forms import UserCreationForm
 from django.urls import reverse_lazy
 from django.views.generic import CreateView
 
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
+class SignupForm(UserCreationForm):
+    class Meta:
+        model = User
+        fields = ("username", "email", "password1", "password2") # Include email field manually which is not in UserCreationForm
+
+
 class SignUpView(CreateView):
-    form_class = UserCreationForm
+    form_class = SignupForm
     success_url = reverse_lazy("login")
     template_name = "registration/signup.html"
