7fe24b401ddbf278eb63f4ff503508a9c03e2a05
TechStuffBoy <nandhu03.m@gmail.com>
2022-07-16 10:23:37 +0530

    Add api-root url.

--- addressbook/api/urls.py
+++ addressbook/api/urls.py
@@ -4,4 +4,5 @@ from addressbook.api import views as views_v1
 urlpatterns = [
     path('', view=views_v1.ContactList.as_view(), name=views_v1.ContactList.name),
     path('<int:pk>/', view=views_v1.ContactDetail.as_view(), name=views_v1.ContactDetail.name),
+    path('api-root/', view=views_v1.ApiRoot.as_view(), name=views_v1.ApiRoot.name),
 ]
